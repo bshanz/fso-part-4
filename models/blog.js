02c3a09bb1f1b38ef009a3eed5e52af216d16a5d
“Brian <90475729+bshanz@users.noreply.github.com>
2023-07-31 16:27:07 -0400

    Exercise 4.8 done

--- models/blog.js
+++ models/blog.js
@@ -16,11 +16,24 @@ mongoose.connect(url)
   })
 
   const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-  })
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        required: true
+    }
+});
+
 
   blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,6 +4,40 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+
+// initialBlogs can be an array of blog objects that you want to add to the database
+const initialBlogs = [
+    {
+        title: "Blog Title",
+        author: "Author 1",
+        url: "http://example.com/blog1",
+        likes: 10
+    },
+    {
+        title: "Blog 2",
+        author: "Author 2",
+        url: "http://example.com/blog2",
+        likes: 20
+    },
+    // add more blogs as needed
+]
+
+beforeEach(async () => {
+    // delete all existing blogs
+    await Blog.deleteMany({})
+
+    // add initial blogs
+    // Note: we're using Promise.all to ensure that all blogs have been saved
+    // before we proceed with the tests. Promise.all takes an array of promises
+    // and returns a new promise that only resolves when all the input promises have resolved.
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+// test cases go here
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -11,10 +45,10 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('there is one note', async () => {
+test('there are two blogs', async () => {
     const response = await api.get('/api/blogs')
   
-    expect(response.body).toHaveLength(1)
+    expect(response.body).toHaveLength(2)
   })
 
 test('First blog title is "Blog Title', async () =>{
@@ -22,6 +56,43 @@ test('First blog title is "Blog Title', async () =>{
 
     expect(response.body[0].title).toBe("Blog Title")
 })
+
+// test('Blog created successfully', async () => {
+//     const newBlog = {
+//         title: "New Blog",
+//         author: "JK Rowling",
+//         url: "http://google.com",
+//         likes: 100
+//     }
+
+//     const response = await api.post('/api/blogs', newBlog)
+
+//     expect(response.status).toBe(201)
+//     expect(response.body.id).toBeDefined() // new
+//     const { id, ...blogWithoutId } = response.body;
+//     expect(blogWithoutId).toMatchObject(newBlog)
+// })
+
+test('Blog without required content is not added', async () => {
+    // You may replace 'title' with 'author', 'url', or 'likes' to test other fields
+    const newBlog = {
+        author: "JK Rowling",
+        url: "http://google.com",
+        likes: 100
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+    const response = await api.get('/api/blogs')
+
+    // replace initialNotes with initialBlogs (or whatever the initial blog list is called)
+    expect(response.body).toHaveLength(2)
+})
+
+
 
 afterAll(async () => {
   await mongoose.connection.close()
